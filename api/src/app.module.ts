8505632e05d33e7b19b0e2e68db4664e36ebc45e
alexeh <alexurola@gmail.com>
2025-12-17 05:11:53 +0100

    llm module scaffolding

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,6 +7,8 @@ import { ApiConfigService } from './modules/config/api-config.service';
 import { UsersModule } from './modules/users/users.module';
 import { AssignmentsModule } from './modules/assignments/assignments.module';
 import { InputsModule } from './modules/inputs/inputs.module';
+import { LlmModule } from './modules/llm/llm.module';
+import { LlmService } from './modules/llm/llm.service';
 
 @Module({
   imports: [
@@ -19,8 +21,9 @@ import { InputsModule } from './modules/inputs/inputs.module';
     UsersModule,
     AssignmentsModule,
     InputsModule,
+    LlmModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, LlmService],
 })
 export class AppModule {}
