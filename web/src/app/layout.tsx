62ed6edffee26907559257fe494256267f53a479
alexeh <alexurola@gmail.com>
2025-12-30 13:34:25 +0100

    fix deprecated middleware

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 import { Providers } from "@/components/providers";
 
 export const metadata: Metadata = {
-  title: "IrakasleArbel",
-  description: "Irakasleentzako kudeaketa arbela",
+  title: "IKASI AI",
+  description: "Kudeaketa Arbela",
 };
 
 export default function RootLayout({
